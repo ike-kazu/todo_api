1e3f72b057b201acdc75531a188a8aa3d15994d4
ike-kazu <ike.forwork@gmail.com>
2021-03-23 17:24:40 +0900

    each person could get todos made by him

--- ToDo_2/views.py
+++ ToDo_2/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from .models import User, Todo
 from .serializers import TodoSerializer, UserSerializer
 
@@ -8,8 +9,13 @@ from .serializers import TodoSerializer, UserSerializer
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
     # filter_fields先がリレーションのあるテーブルだった場合はpk打ち込みで検索
     filter_fields = ('user', 'title')
+
+    def get_queryset(self):
+        if self.request.method in SAFE_METHODS:
+            return Todo.objects.filter(user=self.request.user)
 
 
 class UserViewSet(viewsets.ModelViewSet):
